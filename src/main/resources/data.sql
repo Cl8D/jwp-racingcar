5cf0ac252c13bc782f001d5b1aef831952634fb3
jiwon <ljwon77@daum.net>
2023-04-20 11:58:22 +0900

    chore: sql 스크립트 보기 방식을 변경한다

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,50 +1,17 @@
-CREATE TABLE IF NOT EXISTS RACE_RESULT
-(
-    `race_result_id` BIGINT
-(
-    20
-) UNIQUE NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS RACE_RESULT(
+    `race_result_id` BIGINT(20) UNIQUE NOT NULL AUTO_INCREMENT,
     `trial_count` int NOT NULL,
-    `winners` VARCHAR
-(
-    50
-) NOT NULL,
+    `winners` VARCHAR(50) NOT NULL,
     `created_at` DATETIME NOT NULL default current_timestamp,
-    PRIMARY KEY
-(
-    `race_result_id`
-)
+    PRIMARY KEY(`race_result_id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE =utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS CAR
-(
-    `car_id` BIGINT
-(
-    20
-) UNIQUE NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR
-(
-    10
-) NOT NULL,
+CREATE TABLE IF NOT EXISTS CAR(
+    `car_id` BIGINT(20) UNIQUE NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(10) NOT NULL,
     `position` int NOT NULL,
-    `race_result_id` BIGINT
-(
-    20
-),
-    PRIMARY KEY
-(
-    `car_id`
-),
-    UNIQUE uk_car_name_race_result_id
-(
-    `name`,
-    `race_result_id`
-),
-    FOREIGN KEY
-(
-    `race_result_id`
-) REFERENCES RACE_RESULT
-(
-    `race_result_id`
-)
+    `race_result_id` BIGINT(20),
+    PRIMARY KEY(`car_id`),
+    UNIQUE uk_car_name_race_result_id(`name`,`race_result_id`),
+    FOREIGN KEY(`race_result_id`) REFERENCES RACE_RESULT(`race_result_id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE =utf8mb4_general_ci;
